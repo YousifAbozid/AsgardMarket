b29ed61ec9e90d7bd93141c50e2f58a096cf6261
Yousif Abozid <yousif.abozid@yahoo.com>
2021-02-16 08:12:21 +0200

    add users page

---
+++ pages/users.js
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+const users = () => {
+    return (
+        <div>
+            <Head>
+                <title>Asgard Market - Users</title>
+            </Head>
+            users
+        </div>
+    )
+}
+
+export default users
