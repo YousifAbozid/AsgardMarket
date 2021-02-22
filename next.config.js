a54ea4cc262c5dd37d591128f5a15e8326b38ef7
Yousif Abozid <yousif.abozid@yahoo.com>
2021-02-22 09:15:03 +0200

    reveal secrets :(((

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    env: {
+        BASE_URL: "https://asgard-market.vercel.app",
+        MONGODB_URL:
+            "mongodb+srv://yousif:1231999@cluster0.ldd8g.mongodb.net/asgard-market?retryWrites=true&w=majority",
+        ACCESS_TOKEN_SECRET:
+            "praMgT6WnB387mF7am9LL2Ze2XC3eNwBq2XVfb2VJBVVaCU6YJ",
+        REFRESH_TOKEN_SECRET:
+            "Wg753fRkJggSLeHKSWsBkmJgtKxsudjtNTgcayc9sqSWKwpehBCTyAUtRuAs9tQSBfLFXy8QBH54HcJY",
+        SB_CLIENT_ID:
+            "AUc5z3Pw0GrcfWZSHFtgRFIoUsfoxFsMackLMnoJOYFka2cKKiMWWHBoulbuytYdvUzQYyrQJhdvIlX3",
+        CLOUD_UPDATE_PRESET: "Asgard-Market",
+        CLOUD_NAME: "fswd",
+        CLOUD_API: "https://api.cloudinary.com/v1_1/fswd/image/upload",
+    },
+}
